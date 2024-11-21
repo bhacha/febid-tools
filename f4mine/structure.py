90adb8599b358d2f1c54b5c9b38f58584b22929a
Brandon Hacha <29590546+bhacha@users.noreply.github.com>
2024-11-21 17:46:16 -0600

    beginning thermal model

--- f4mine/structure.py
+++ f4mine/structure.py
@@ -5,18 +5,16 @@ import skimage.graph as sg
 import scipy.ndimage as snd
 np.set_printoptions(threshold=np.inf)
 
-### Harmonic series approximate sum
-def harmonic(f):
-    """Since parallel resistance adds inverses, I am using the Euler-Mascheroni equation to quickly calculate the total"""
-    f = round(f)
-    if f>0:
-        invres = ((np.log(f) + .5772156649) + 1/(2*f)) #Euler-Mascheroni
-        
-        
-    else:
-        res = 0
-        
-    return res
+class SEM:
+    def __init__(
+            self,
+            addressable_pixels,
+            screen_width
+    ):
+        self.addressable_pixels = addressable_pixels
+        self.screen_width = screen_width
+        self.field_center = [addressable_pixels[0]/2, addressable_pixels[1]/2]
+        self.pixel_size = screen_width/addressable_pixels[0] 
 
 def import_numpy(filepath):
     epsilon_array = np.load(filepath)
@@ -52,8 +50,14 @@ def binarize(array, threshold):
     binary_array = np.where(array>threshold, False, True)
     return binary_array
 
+
+
+"""
+f3ast and my other functions use a layer-by-layer approach. I can't help but feel like a kernel convolve/correlate approach is better. My thinking is that an x-y proximity matrix can be represented via weights in one row, while resistances can be represented by weights in columns. A wide, interconnected structure should have more adjacent pixels/voxels and thus would have larger sums. Of course, the resulting matrix would have a lot of meaningless weights in areas that are not supposed to be fabricated, but the array could be used as a lookup table of sorts. The indices of the nonzero pixels in binary_array could be used to query the same points in the convolved matrix and return the resistance or proximity weight. 
+"""
+
 def calculate_neighbors_kernel(binary_array, pitch, sigma):
-    """ I'm abandoning this in favor the other options"""
+
     """
     Param
     -------
@@ -78,6 +82,26 @@ def calculate_neighbors_kernel(binary_array, pitch, sigma):
 
     return neighbor_arr
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####
+"""
+The below functions use layer-by-layer calculations and loops to come up with a resistance based on areas and heights.
+"""
 def calculate_regions_in_slices(binary_array, return_bounds=False):
     """
     Returns the array with the same shape as binary_array but with nonzero elements for regions that are connected in the layer.
@@ -222,13 +246,65 @@ def calculate_resistance(binary_array, layer_height = 1):
 
     return resistances
 
+def calculate__index_size(binary_array, SEM, structure_size_nm):
+    """
+    returns the actual size (in pixels) of each index unit in an array, based on the desired fabrication size
+
+
+    Parameters
+    -----------
+        binary_array :
+            array
+
+        SEM : SEM() object
+            object of SEM class, containing the settings 
+
+        structure_size_nm : 
+            desired structure size (scale)
+
+    Returns
+    -----------
+        array_pitch : 
+            size of each index step in an array, in pixel units. Multiply index positions by this number to get pixel coordinates
+    
+    
+    """
+    
+    ##get pixel size
+    pix_size = SEM.pixel_size
+
+    ## convert nm structure size to pixels
+    structure_size = structure_size_nm/pix_size
+
+    ## generate range of addressed pixels
+    structure_xspan = [int(SEM.field_center[0] - structure_size/2), int(SEM.field_center[0] + structure_size/2)]
+    structure_yspan = [int(SEM.field_center[1] - structure_size/2), int(SEM.field_center[1] + structure_size/2)]
+
+    ### Right now, assume the pixels are square. This is probably something that can be fixed later though.
+    array_pitch = structure_size / np.max(binary_array.shape[:1])
+
 def calculate_dwells(binary_array, layer_height, growth_rate, k, sigma):
 
     resistance_array = calculate_resistance(binary_array)
+    # print(np.unique(resistance_array))
 
 
-    dwell_array = growth_rate
+    # create a new array, which uses the fabrication binary_array as a mask for the resistances
+    fabspot_array = np.where(binary_array != 0, resistance_array, 0)
+
+    return fabspot_array
     
+
+def dwell_model(layer_height, growth_rate, k, sigma, resistance, r):
+    
+    ### There's some proximity matrix term that accounts for the gaussian spot, but I don't know how to implement that yet
+
+    growth_term = growth_rate*np.exp((-k*resistance)) 
+    gauss_term = np.exp(-(r**2)/(2*sigma**2))
+
+    dwell_time = layer_height/(growth_term*gauss_term)
+    
+    return dwell_time
 
 
 
@@ -242,6 +318,23 @@ if __name__ == "__main__":
     growth_rate = 200 #nm/s
     k_term = 1.1 #nm
 
+    dwell_list = []
+    for n in np.linspace(0, 2, 30):
+            dwell = dwell_model(layer_height, growth_rate, k_term, sigma=4, resistance=n, r=4)
+            dwell_list.append(dwell)
+            plt.scatter(n, dwell)
+
+    
+    plt.xlabel("Resistance")
+    plt.ylabel("Dwell Time")
+    plt.show()
+    bins = 900
+    dwell_sum = np.sort(dwell_list)
+    dwell_fun = np.array(range(bins))/float(bins)
+
+    plt.figure()
+    plt.plot(dwell_sum, dwell_fun)
+    plt.show()
 
     ## Add a block to help orient things
     # binary_array[30:90, 50:90, :20] = True
