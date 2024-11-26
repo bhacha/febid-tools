518a661735de96bf57d54a313bab15b9b1fd61e4
Brandon Hacha <29590546+bhacha@users.noreply.github.com>
2024-11-26 16:01:48 -0600

    modified:   .gitignore 	new file:   f4mine/branchtrio-SizedUp.stl 	new file:   f4mine/model-noresistance.py 	modified:   f4mine/model.py 	new file:   f4mine/structure-wip.py 	new file:   f4mine/structure_import.py

---
+++ f4mine/model-noresistance.py
@@ -0,0 +1,45 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####
+
+if __name__ == "__main__":
+    print("This is main")
+    # npyfile = "SRSNetwork-r0.002.npy"
+    # test_array = np.load(npyfile)
